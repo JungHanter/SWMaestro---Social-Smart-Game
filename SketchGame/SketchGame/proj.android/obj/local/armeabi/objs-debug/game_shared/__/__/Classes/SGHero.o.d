60c38647863ebdcf80e211ee069f8011a8577d27
Hanter Jung <hanterkr@gmail.com>
2012-09-08 21:24:48 +0900

    android
    code

--- SketchGame/SketchGame/proj.android/obj/local/armeabi/objs-debug/game_shared/__/__/Classes/SGHero.o.d
+++ SketchGame/SketchGame/proj.android/obj/local/armeabi/objs-debug/game_shared/__/__/Classes/SGHero.o.d
@@ -229,7 +229,7 @@ obj/local/armeabi/objs-debug/game_shared/__/__/Classes/SGHero.o: \
  /Installation/cocos2d/cocos2d-2.0-rc2-x-2.0.1/cocos2dx/CCDrawingPrimitives.h \
  /Installation/cocos2d/cocos2d-2.0-rc2-x-2.0.1/cocos2dx/CCScheduler.h \
  /Installation/cocos2d/cocos2d-2.0-rc2-x-2.0.1/cocos2dx/support/data_support/uthash.h \
- jni/../../Classes/defines.h
+ jni/../../Classes/SGMonster.h jni/../../Classes/defines.h
 
 jni/../../Classes/SGHero.h:
 
@@ -690,5 +690,7 @@ jni/../../Classes/SGHero.h:
 /Installation/cocos2d/cocos2d-2.0-rc2-x-2.0.1/cocos2dx/CCScheduler.h:
 
 /Installation/cocos2d/cocos2d-2.0-rc2-x-2.0.1/cocos2dx/support/data_support/uthash.h:
+
+jni/../../Classes/SGMonster.h:
 
 jni/../../Classes/defines.h:
