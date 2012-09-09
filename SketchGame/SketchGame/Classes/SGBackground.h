d6877c639f238b2624cae4b34da7ce197a4aa585
Hanter Jung <hanterkr@gmail.com>
2012-09-09 19:46:18 +0900

    bg modifying..
    배경추가수정중..에잠듬ㅋ

--- SketchGame/SketchGame/Classes/SGBackground.h
+++ SketchGame/SketchGame/Classes/SGBackground.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //
 //  SGBackground.h
 //  SketchGame_TouchInput
@@ -44,3 +45,53 @@ public:
 };
 
 #endif /* defined(__SketchGame_TouchInput__SGBackground__) */
+=======
+//
+//  SGBackground.h
+//  SketchGame_TouchInput
+//
+//  Created by Jung Hanter on 12. 9. 1..
+//
+//
+
+#ifndef __SketchGame_TouchInput__SGBackground__
+#define __SketchGame_TouchInput__SGBackground__
+
+#include "cocos2d.h"
+using namespace cocos2d;
+
+class SGBackground : public CCObject {
+
+public:
+    void pauseAllBackground();
+    void resumeAllBackground();
+
+private:
+    CCSprite* bg_map;
+    CCActionInterval* bg_map_action;
+    
+    CCSprite* bg_overlay;
+    CCActionInterval* bg_forest_action, *bg_forest_into_action;
+    CCActionInterval* bg_cave_action, *bg_cave_into_action, *bg_cave_out_action;
+    
+    CCSprite* bg_mountain, *bg_mountain2;
+    CCSprite* bg_cloud, *bg_cloud2;
+    CCSprite* bg_castle;
+    
+    CCLayer* const parentLayer;
+    
+private:
+    void func_mountainMove();
+    void func_cloudMove();
+    
+private:
+    SGBackground(CCLayer* parent);
+    static SGBackground* sharedSGBackground;
+    
+public:
+    virtual ~SGBackground();
+    static SGBackground* sharedInstance(CCLayer *parent);
+};
+
+#endif /* defined(__SketchGame_TouchInput__SGBackground__) */
+>>>>>>> bg modifying..
